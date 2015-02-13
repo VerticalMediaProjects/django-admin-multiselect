54ac62f0bcb3a3104eaf622b0a7daa59c8ee11cd
Gabriel - Iulian Dumbrava <gabriel.dumbrava@gmail.com>
2015-02-13 15:39:47 +0200

    Changed github repo.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import os
 from setuptools import setup, find_packages
 setup(
     name='django-admin-multiselect',
-    version='0.0.2',
+    version='0.0.3',
     description='Alternative multiple select widget that works with mobile devices.',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
     author='Kreios S.a r.l.',
     author_email='christian.hillerkus@kreios.lu',
-    url='https://github.com/ckesselh/django-admin-multiselect',
-    download_url='https://github.com/ckesselh/django-admin-multiselect/tarball/0.0.2',
+    url='https://github.com/kreioslu/django-admin-multiselect',
+    download_url='https://github.com/kreioslu/django-admin-multiselect/tarball/0.0.3',
     packages=find_packages(),
     classifiers=[
         'Environment :: Web Environment',
