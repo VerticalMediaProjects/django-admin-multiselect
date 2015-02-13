87a51713a3ddc4595c82dc1e6cb67cc5c328eaf5
Gabriel - Iulian Dumbrava <gabriel.dumbrava@gmail.com>
2015-02-13 16:34:33 +0200

    Fixed long description

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ import os
 from setuptools import setup, find_packages
 setup(
     name='django-admin-multiselect',
-    version='0.0.4',
+    version='0.0.5',
     description='Alternative multiple select widget that works with mobile devices.',
-    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
     author='Kreios S.a r.l.',
     author_email='christian.hillerkus@kreios.lu',
     url='https://github.com/kreioslu/django-admin-multiselect',
-    download_url='https://github.com/kreioslu/django-admin-multiselect/tarball/0.0.4',
+    download_url='https://github.com/kreioslu/django-admin-multiselect/tarball/0.0.5',
     packages=find_packages(),
     classifiers=[
         'Environment :: Web Environment',
